4122b858f00fac05445e74b54918b867bac36973
Sobir <sobirdzhon.t@gmail.com>
2024-06-26 22:45:44 -0500

    api login

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,11 @@
+import { login } from "@/actions/auth/login";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function POST(request: NextRequest) {
+  const { error, success, data } = await login(await request.json());
+  if (error) return NextResponse.json({ error, data }, { status: 401 });
+  if (success) return NextResponse.json({ success }, { status: 200 });
+  return NextResponse.json({ error: "Something went wrong." }, { status: 500 });
+}
