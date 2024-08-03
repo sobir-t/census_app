7b78e158925143afaf6c80c1acf68d7d9672a836
Sobir <sobirdzhon.t@gmail.com>
2024-08-02 21:22:18 -0500

    removing env vars in ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,6 @@ module.exports = {
       script: "npm start",
       env_production: {
         NODE_ENV: "production",
-        DATABASE_URL: process.env.DATABASE_URL,
-        AUTH_SECRET: process.env.AUTH_SECRET,
-        AUTH_TRUST_HOST: true,
-        AUTH_URL: `${process.env.SSH_HOSTMACHINE}/`,
-        HOSTNAME: process.env.SSH_HOSTMACHINE,
       },
     },
   ],
