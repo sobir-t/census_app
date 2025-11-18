556e70b06a0227279143e08d715f0add5a14f19b
Sobir <sobirdzhon.t@gmail.com>
2025-11-17 21:27:49 -0600

    tailwind update

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -1,6 +1,6 @@
 export default function PageHeader({ title }: { title: string }) {
   return (
-    <header className="bg-white shadow">
+    <header className="bg-white shadow-sm">
       <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold tracking-tight text-gray-900" data-testid="header-title">
           {title}
